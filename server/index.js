136a94f7554920b519e215d2f479ca3d7a13ccd2
kc122307 <kunalchandel1223@gmail.com>
2025-06-28 00:28:12 +0530

    Demo Version Ready

--- server/index.js
+++ server/index.js
@@ -9,10 +9,7 @@ app.use(cors());
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
